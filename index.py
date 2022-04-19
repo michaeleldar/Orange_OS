0f49f019c72f60bd4c06305b3dfe7b36ed80bc4c
Michael Halpin <halpinm2010@gmail.com>
2022-04-19 19:39:32 +0100

    .

--- index.py
+++ index.py
@@ -2,27 +2,36 @@ import scratchconnect
 from mypass import my_pass
 import time
 import os
-from contextlib import suppress
-
 user = scratchconnect.ScratchConnect("applejuice_alt", my_pass)
 project = user.connect_project(project_id=677519831)
 variables = project.connect_cloud_variables()
-print(variables.get_cloud_variable_value("cloud", limit=2))
 
 
 while True:
-    with suppress(Exception):
+    buffer_name = variables.get_cloud_variable_value("cloud", limit=2)
+    buffer_contents = variables.get_cloud_variable_value("clou2", limit=2)
+    buffer_sender = variables.get_cloud_variable_value("user", limit=2)
+
+    if buffer_name == []:
+        buffer_name = ""
+    else:
         buffer_name = variables.get_cloud_variable_value("cloud", limit=2)[0]
+    if buffer_contents == []:
+        buffer_contents = ""
+    else:
         buffer_contents = variables.get_cloud_variable_value("clou2", limit=2)[0]
+    if buffer_sender == []:
+        buffer_sender = ""
+    else:
         buffer_sender = variables.get_cloud_variable_value("user", limit=2)[0]
-        if buffer_name != " " and buffer_contents != "" and buffer_sender != "":
-            variables.set_cloud_variable("cloud", "")
-            variables.set_cloud_variable("clou2", "")
-            variables.set_cloud_variable("user", "")
-            try:
-                os.mkdir(str(buffer_sender))
-            except:
-                pass
-            file = open(str(buffer_sender) + str(buffer_name), 'w')
-            file.write(str(buffer_contents))
+    if buffer_name != " " and buffer_contents != "" and buffer_sender != "":
+        variables.set_cloud_variable("cloud", "")
+        variables.set_cloud_variable("clou2", "")
+        variables.set_cloud_variable("user", "")
+        try:
+            os.mkdir(str(buffer_sender))
+        except:
+            pass
+        file = open(str(buffer_sender) + str(buffer_name), 'w')
+        file.write(str(buffer_contents))
         
