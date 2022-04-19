daf817918fd05e4771827104ddf3a3a4dcaa01a5
Michael Halpin <halpinm2010@gmail.com>
2022-04-19 09:54:11 +0100

    .

--- index.py
+++ index.py
@@ -1,10 +1,11 @@
 import scratchconnect
 from mypass import my_pass
 import time
+import os
 
 user = scratchconnect.ScratchConnect("applejuice_alt", my_pass)
 project = user.connect_project(project_id=677505040)
 variables = project.connect_cloud_variables()
 variables.get_variable_data(limit=100, offset=0)
 while True:
-    
+    print(variables.client_username)
