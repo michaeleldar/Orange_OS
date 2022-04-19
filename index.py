af45c823592bfdae04c89ed68bef0c28fbdda1f7
Michael Halpin <halpinm2010@gmail.com>
2022-04-19 09:20:47 +0100

    .x

--- index.py
+++ index.py
@@ -2,6 +2,8 @@ from django.db import connection
 from requests import session
 from scratchclient import ScratchSession
 from mypass import my_pass
+import time
 session = ScratchSession("applejuice_alt", my_pass)
 connection = session.create_cloud_connection(677505040)
+time.sleep(5)
 connection.set_cloud_variable("my variable", 5000)
