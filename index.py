9a4233e04365aff995d2aa8663808a5c552a1092
Michael Halpin <halpinm2010@gmail.com>
2022-04-19 09:10:23 +0100

    .

--- index.py
+++ index.py
@@ -1,4 +1,7 @@
-import scratchconnect
-import mypass
-# I'm not gonna reveal my password lol.
-user = scratchconnect.ScratchConnect("applejuice_alt", mypass.my_pass)
+from django.db import connection
+from requests import session
+from scratchclient import ScratchSession
+from mypass import my_pass
+session = ScratchSession("applejuice_alt", my_pass)
+connection = session.create_cloud_connection(677505040)
+connection.set_cloud_variable("my variable", 5000)
