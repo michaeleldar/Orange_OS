6c3b83e15b93ac7e3a518ba3f424496080aa6e61
Michael Halpin <halpinm2010@gmail.com>
2022-04-19 09:35:51 +0100

    .

--- index.py
+++ index.py
@@ -6,4 +6,8 @@ user = scratchconnect.ScratchConnect("applejuice_alt", my_pass)
 project = user.connect_project(project_id=677505040)
 variables = project.connect_cloud_variables()
 variables.get_variable_data(limit=100, offset=0)
-print(variables.get_cloud_variable_value(variable_name="my variable", limit=1000))
+
+if variables.get_cloud_variable_value(variable_name="my variable", limit=1000)[0] == 0:
+    print("message from scratch")
+    time.sleep(5)
+    variables.set_cloud_variable(variable_name="my variable", value=1)
