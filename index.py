fd35c66730edd91a72aa8ff89246a9f25c95c5cb
Michael Halpin <halpinm2010@gmail.com>
2022-04-19 15:37:25 +0100

    .

--- index.py
+++ index.py
@@ -6,9 +6,13 @@ import os
 user = scratchconnect.ScratchConnect("applejuice_alt", my_pass)
 project = user.connect_project(project_id=677519831)
 variables = project.connect_cloud_variables()
+print(variables.get_cloud_variable_value("cloud", limit=2))
 
 
 while True:
-    buffer_name = print(variables.get_cloud_variable_value("cloud", limit=2))
-    buffer_contents = print(variables.get_cloud_variable_value("clou2", limit=2))
+    buffer_name = variables.get_cloud_variable_value("cloud", limit=2)
+    buffer_contents = variables.get_cloud_variable_value("clou2", limit=2)
     if buffer_name != "" and buffer_contents != "":
+        variables.set_cloud_variable("cloud", "")
+        variables.set_cloud_variable("clou2", "")
+        
