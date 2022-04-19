1826b2454f62fc4de18b9915c0a889f3600eaa91
Michael Halpin <halpinm2010@gmail.com>
2022-04-19 09:42:42 +0100

    .

--- index.py
+++ index.py
@@ -7,10 +7,4 @@ project = user.connect_project(project_id=677505040)
 variables = project.connect_cloud_variables()
 variables.get_variable_data(limit=100, offset=0)
 while True:
-    print("in loop")
-    print(variables.get_cloud_variable_value(variable_name="my variable", limit=1000))
-    if variables.get_cloud_variable_value(variable_name="my variable", limit=1000)[0] == "1":
-        print("message from scratch")
-        time.sleep(5)
-        variables.set_cloud_variable(variable_name="my variable", value=2)
-        quit()
+    
