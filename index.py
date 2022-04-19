69a80a151716119a4947295424be30c8da244389
Michael Halpin <halpinm2010@gmail.com>
2022-04-19 10:05:48 +0100

    .

--- index.py
+++ index.py
@@ -6,9 +6,11 @@ import os
 user = scratchconnect.ScratchConnect("applejuice_alt", my_pass)
 project = user.connect_project(project_id=677519831)
 variables = project.connect_cloud_variables()
-variables.get_variable_data(limit=100, offset=0)
 while True:
     if os.path.isdir("./" + variables.client_username) == True:
         pass
     else:
         os.mkdir("./" + variables.client_username)
+    while variables.get_cloud_variable_value(variable_name="cloud")[0] == 0:
+        continue
+    file = open("./" + variables.client_username + "/" + variables.get_cloud_variable_value(variable_name="cloud")[0])
